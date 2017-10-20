f6cfc4f0c832d63347dff023901c6ba58c7ea11c
Maeve Murphy <maevemurphy530@gmail.com>
2017-10-20 15:33:03 -0700

    refined importData and also added inserting into 'students'

--- teamworkApp/lib/importData.py
+++ teamworkApp/lib/importData.py
@@ -1,5 +1,6 @@
 import sqlite3
 import csv
+from datetime import datetime, date, time
 from enum import IntEnum
 
 DB = 'db/development.sqlite3'
@@ -17,74 +18,126 @@ def getAnswer(answerNum):
 	raise ValueError("Invalid response")
 
 def processAnswerData(csvFileName):
-	"""Given the number of students and a csv file, importAnswerData will
+	"""Given a csv file, importAnswerData will
 	obtain the answers for the number of students given for 18 questions
 	and return it as a dictionary the size of the number of students with 18
 	responses.  Each response is originally comprised of 4 parts but is
 	summarized into one of the 24 possible responses."""
-	processedData = {}
 	with open(csvFileName) as csvfile:
+
+		timestamps = []
+		expandedResponses = []
+		studentResponses = []
+
 		answerReader = csv.reader(csvfile)
+
 		studentData = list(answerReader)
 		studentData = studentData[1:]
-		timestamps = []
-		studentResponses = []
+
 		for student in studentData:
+			# these are the 18x4 responses to the survey
 			teamworkResponses = student[1:73]
+			# the timestamps are saved in the first column
 			timestamps += [student[0]]
-			listOfStrings = []
+			aggregateResponses = []
 			enummedResponses = []
 			count = 0
 			while (count < 72):
-				listOfStrings += [teamworkResponses[count] +
-								 teamworkResponses[count+1] +
-								 teamworkResponses[count+2] +
-								 teamworkResponses[count+3]]
+				aggregateResponses += [teamworkResponses[count] +
+								       teamworkResponses[count+1] +
+								       teamworkResponses[count+2] +
+								       teamworkResponses[count+3]]
 				count += 4
-			for resp in listOfStrings:
+			expandedResponses += [aggregateResponses]
+
+			for resp in aggregateResponses:
 				ans = getAnswer(resp)
 				enummedResponses += [ans]
 			studentResponses.append(enummedResponses)
-		return timestamps, studentResponses
+
+		return expandedResponses, timestamps, studentResponses
+
+def findStyle(expandedResponses):
+	contributor = 0
+	collaborator = 0
+	communicator = 0
+	challenger = 0
+	tracker = 0
+	for resp in expandedResponses:
+		# this pattern is based on the teamwork survey scoring scheme
+		if tracker == 0:
+			contributor += int(resp[0]) # a
+			collaborator += int(resp[1]) # b
+			communicator += int(resp[2]) # c
+			challenger += int(resp[3]) # d
+		elif tracker == 1:
+			contributor += int(resp[3]) # d
+			collaborator += int(resp[0]) # a
+			communicator += int(resp[1]) # b
+			challenger += int(resp[2]) # c
+		elif tracker == 2:
+			contributor += int(resp[2]) # c
+			collaborator += int(resp[3]) # d
+			communicator += int(resp[0]) # a
+			challenger += int(resp[1]) # b
+		elif tracker == 3:
+			contributor += int(resp[1]) # b
+			collaborator += int(resp[2]) # c
+			communicator += int(resp[3]) # d
+			challenger += int(resp[0]) # a
+		else:
+			tracker = -1
+		tracker += 1
+	# currently does not account for having multiple dominant styles...
+	if contributor == max(contributor, collaborator, communicator, challenger):
+		return Style(0).name
+	elif collaborator == max(contributor, collaborator, communicator, challenger):
+		return Style(1).name
+	elif communicator == max(contributor, collaborator, communicator, challenger):
+		return Style(2).name
+	else:
+		return Style(3).name
+
 
 def insertAnswerData(csvFileName):
-	"""Import data from the csv file into the specified
-	table within out SQLite3 database.
+	"""Import data from the csv file into answers and students
+	within out SQLite3 database.
 	csvFileName: name (possibly including path) of the
-		with the desired data
-	tableName: table where data is to be stored
-	col1: starting column
-	col2: ending column"""
-# TODO(maeve): refine this whole file
-	timestamps, studentResponses = processAnswerData(csvFileName)
+		with the desired data"""
+	expandedResponses, timestamps, studentResponses = processAnswerData(csvFileName)
+	to_db = []
+	student_to_db = []
 	count = 0
+
 	for students in studentResponses:
 		questionCount = 0
 		studentId = 'S' + str(count)
+
 		for response in students:
-			to_db = [response, timestamps[count], timestamps[count], studentId, questionCount]
-			conn = sqlite3.connect(DB)
-			c = conn.cursor()
-			c.execute('INSERT INTO answers (value, created_at, updated_at, student_id, question) VALUES (?, ?, ?, ?, ?);', to_db)
-			conn.commit()
-			conn.close()
+			to_db += [(response, timestamps[count], timestamps[count], questionCount)]
 			questionCount += 1
+
+		now = datetime.now().isoformat()
+		student_to_db += [(findStyle(expandedResponses[count]), 0, timestamps[count], now)]
 		count += 1
 
-# generate a bunch of data and shove it into answers
-def main():
+	print("Inserting data into 'answers'")
+	print("Inserting data into 'students'")
 
 	conn = sqlite3.connect(DB)
 	c = conn.cursor()
-
-	c.execute('SELECT * FROM students;')
-	# insertAnswerData('lib/test_spreadsheet.csv')
-	# c.execute('SELECT * FROM answers;')
-
-	print(c.fetchone())
-
+	# insert student data for each question for the student
+	c.executemany('INSERT INTO answers (value, created_at, updated_at,question) VALUES (?, ?, ?, ?);', to_db)
+	# insert student data for each student
+	c.executemany('INSERT INTO students (style, team, created_at, updated_at) VALUES (?, ?, ?, ?);', student_to_db)
 	conn.commit()
 	conn.close()
+
+def main():
+
+	print("Inserting data")
+	insertAnswerData('lib/test_spreadsheet.csv')
+	print("Success: Data inserted into tables!")
 
 if __name__ == "__main__":
 	main()
