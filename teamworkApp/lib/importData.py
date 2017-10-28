eeb7601693a66902f110681cecf12a3fc3e88625
Maeve Murphy <maevemurphy530@gmail.com>
2017-10-28 15:40:54 -0700

    updated conventions via Python style guide and added comments

--- teamworkApp/lib/importData.py
+++ teamworkApp/lib/importData.py
@@ -10,82 +10,96 @@
 
 import sqlite3
 import csv
+from evaluateAnswers import *
 from datetime import datetime, date, time
 from enum import IntEnum
 
 DB = 'db/development.sqlite3'
+EXPANDED_QUESTIONS = 72
 Answer_Value = IntEnum(
 	'Answer_Value',
 	'1234 1243 1324 1342 1423 1432 2134 2143 2314 2341 2413 2431 3124 3142 3214 3241 3412 3421 4123 4132 4213 4231 4312 4321',
 	start=0
 )
 
-def getAnswer(answerNum):
-	for ans in range(0, 23):
-		if Answer_Value(ans).name == answerNum:
+
+def get_answer(answer_num):
+	for ans in range(len(Answer_Value)):
+		if Answer_Value(ans).name == answer_num:
 			return ans
 	raise ValueError("Invalid response")
 
-def processAnswerData(csvFileName):
-	"""Given the Teamwork Survey csv file, processAnswerData will extract the
+def process_answer_data(csv_filename):
+	"""Given the Teamwork Survey csv file, process_answer_data will extract the
 	pertinent information and populate a student dictionary with it.
 	This processing requires very specifically formatted .csv files and it
 	assumes they come from the results of the Teamwork Survey sent out by
 	muddersOnRails.
-	input csvFilename: name (and possibly path to) the csv file corresponding
+	input csv_filename: name (and possibly path to) the csv file corresponding
 		to the data received by respondants to the Teamwork Survey
-	output studentDict: a dictionary. The keys are usernames of students
+	output student_dict: a dictionary. The keys are usernames of students
 		collected from the Teamwork Survey. The value for each key is a list of
 		three pieces of information:
-			[timestamp, expandedResponses, enummedResponses]
+			[timestamp, expanded_responses, enummed_responses]
 			timestamp: the time at which the student filled out the Teamwork
 				Survey.
-			expandedResponses: the responses to each question expanded out into
+			expanded_responses: the responses to each question expanded out into
 				a string of the four-digit integer corresponding to their
 				rankings, this is a list of 18 responses.
-			enummedResponses: the list of responses to each question enumerated
+			enummed_responses: the list of responses to each question enumerated
 				according to the IntEnum Answer_Value. This is a list of 18
 				integers corresponding to the IntEnum."""
-	with open(csvFileName) as csvfile:
+	with open(csv_filename) as csvfile:
 
-		timestamps        = []
-		expandedResponses = []
-		studentDict       = {}
+		# the student dictionary has keys of student usernames and values
+			# explained in the docstring
+		student_dict       = {}
 
-		answerReader = csv.reader(csvfile)
-		studentData  = list(answerReader)
+		# read the csv file
+		answer_reader = csv.reader(csvfile)
+		student_data  = list(answer_reader)
 		# get rid of the row with questions
-		studentData  = studentData[1:]
+		student_data  = student_data[1:]
 
-		for student in studentData:
+		for student in student_data:
 			# these are the 18x4 responses to the survey
-			teamworkResponses = student[1:73]
-			expandedResponses = []
-			enummedResponses  = []
+			teamwork_responses = student[1:EXPANDED_QUESTIONS+1]
+			# expanded responses are of the form 'wxyz' where w != x != y != z
+				# and w, x, y, and z are all values between 1 and 4 inclusive
+			expanded_responses = []
+			# enummed responses correspond to an integer between 0 and 23
+				# inclusive where the integer is the value in Answer_Value
+			enummed_responses  = []
 			count = 0
-			while (count < 72):
-				expandedResponses += [ teamworkResponses[count]   +
-								       teamworkResponses[count+1] +
-								       teamworkResponses[count+2] +
-								       teamworkResponses[count+3] ]
+			while (count < EXPANDED_QUESTIONS):
+				# grab the next four digits and "glue" them together
+					# to creat the expanded response
+				expanded_responses += [teamwork_responses[count]   +
+								       teamwork_responses[count+1] +
+								       teamwork_responses[count+2] +
+								       teamwork_responses[count+3] ]
+				# increment by to not quadruple count characters
 				count += 4
-			for resp in expandedResponses:
-				ans = getAnswer(resp)
-				enummedResponses += [ans]
+			# find the corresponding value to this expanded response
+			for resp in expanded_responses:
+				ans = get_answer(resp)
+				enummed_responses += [ans]
 
 			# the timestamps are saved in the first column of the .csv file
 			timestamp = student[0]
-			studentDict[student[-1]] = [timestamp, expandedResponses, enummedResponses]
+			student_dict[student[-1]] = [timestamp, expanded_responses, enummed_responses]
 
-		return studentDict
+		return student_dict
 
-def findStyle(expandedResponses):
+def find_style(expanded_responses):
+	#TODO: This is basically deprecated as we are going to use evaluateAnswers
+		# for this calculation
 	contributor  = 0
 	collaborator = 0
 	communicator = 0
 	challenger   = 0
 	tracker      = 0
-	for resp in expandedResponses:
+	for resp in expanded_responses:
 		# this pattern is based on the teamwork survey scoring scheme
 		if tracker == 0:
 			contributor  += int(resp[0]) # a
@@ -112,76 +126,84 @@ def findStyle(expandedResponses):
 		tracker += 1
 	return communicator, collaborator, challenger, contributor
 
-def listStudentData(studentDict):
-	"""Using data from the csv file given, insertStudentData finds how many
+def liststudent_data(student_dict):
+	"""Using data from the csv file given, insertstudent_data finds how many
 	students are in the file and places them into the student table"""
 	student_to_db = []
 
-	for student in studentDict.keys():
+	for student in student_dict.keys():
 		now = datetime.now().isoformat()
 		# TODO(Maeve): add functionality to get which team the student is on.
 			# This will require more functionality on the teams side
 		# currently name and username are the same
-		student_to_db += [(student, studentDict[student][0], now, student)]
+		student_to_db += [(student, student_dict[student][0], now, student)]
 	return student_to_db
 
-def listAnswerData(studentDict):
-	"""Import data from the csv file into answers and students
-	within out SQLite3 database.
-	csvFileName: name (possibly including path) of the
-		with the desired data"""
+def list_answer_data(student_dict):
+	"""Return the list of answer data corresponding to each student within
+	student_dict.
+	output: answer_to_db is a list of tuples holding information corresponding
+		to the columns within the answers table that is inserted in a future
+		function."""
 	# fetch the names of the students from the table
 	conn = sqlite3.connect(DB)
 	c = conn.cursor()
 	# get the primary keys for the students
 	c.execute('SELECT id FROM students;')
-	tempKeys = c.fetchall()
+	temp_keys = c.fetchall()
 	conn.commit()
 	conn.close()
-	primaryKeys  = [tempKey[0] for tempKey in tempKeys]
+	primary_keys  = [temp_key[0] for temp_key in temp_keys]
 	answer_to_db = []
 	count = 0
 
-	for student in studentDict.keys():
-		now           = datetime.now().isoformat()
-		responses     = studentDict[student][2]
-		questionCount = 0
+	# iterate through the students to find the enumerated responses to each
+		# question along with student_id and relevant timestamps
+	for student in student_dict.keys():
+		now                  = datetime.now().isoformat()
+		enummed_responses     = student_dict[student][2]
+		question_count       = 0
 
-		for response in responses:
+		for response in enummed_responses:
 			answer_to_db += [
 			(response,
-			studentDict[student][0],
+			student_dict[student][0],
 			now,
-			primaryKeys[count],
-			questionCount)
+			primary_keys[count],
+			question_count)
 			]
-			questionCount += 1
+			question_count += 1
 		count += 1
 	return answer_to_db
 
-def listStyleData(studentDict):
+def list_style_data(student_dict):
 	"""Given a dictionary of student data, calculate and import style data
 	for each student.
-	Uses findStyle to calculate styles."""
+	Uses find_style to calculate styles."""
 	style_to_db = []
 	# fetch the names of the students from the table
 	conn = sqlite3.connect(DB)
 	c = conn.cursor()
 	# get the primary keys for the students
 	c.execute('SELECT id FROM students;')
-	tempKeys = c.fetchall()
+	temp_keys = c.fetchall()
 	conn.commit()
 	conn.close()
 
-	primaryKeys = [tempKey[0] for tempKey in tempKeys]
+	primary_keys = [temp_key[0] for temp_key in temp_keys]
 
 	count = 0
-	for student in studentDict.keys():
+	for student in student_dict.keys():
 		now               = datetime.now().isoformat()
-		expandedResponses = studentDict[student][1]
-		communicator, collaborator, challenger, contributor = findStyle(expandedResponses)
-		created_at   = studentDict[student][0]
-		student_id   = primaryKeys[count]
+		expanded_responses = student_dict[student][1]
+		communicator, collaborator, challenger, contributor = find_style(expanded_responses)
+		# TODO: The commented out line below should be what we use in the
+			# future; however, evaluateAnswers has not been updated for the
+			# new schema (as of the time of writing this 10-28-2017) so we
+			# cannot use it yet
+		# (communicator, collaborator, challenger, contributor) = find_scores(primary_keys[count])
+		created_at   = student_dict[student][0]
+		student_id   = primary_keys[count]
 		style_to_db += [
 			(student_id,
 			communicator,
@@ -195,9 +217,9 @@ def listStyleData(studentDict):
 
 	return style_to_db
 
-def executeInsert(csvFileName):
-	studentDict   = processAnswerData(csvFileName)
-	student_to_db = listStudentData(studentDict)
+def execute_insert(csv_filename):
+	student_dict   = process_answer_data(csv_filename)
+	student_to_db = liststudent_data(student_dict)
 	print("Inserting .csv information into 'students'...")
 	conn = sqlite3.connect(DB)
 	c = conn.cursor()
@@ -207,10 +229,10 @@ def executeInsert(csvFileName):
 	conn.close()
 	print("Success.")
 	# answer and style depend on information being stored in students so we
-		# can only call listAnswerData and listStudentData after
-		# listStudentData's information has been inserted
-	answer_to_db  = listAnswerData(studentDict)
-	style_to_db   = listStyleData(studentDict)
+		# can only call list_answer_data and liststudent_data after
+		# liststudent_data's information has been inserted
+	answer_to_db  = list_answer_data(student_dict)
+	style_to_db   = list_style_data(student_dict)
 	print("Inserting data into 'answers' and 'styles'...")
 
 	conn = sqlite3.connect(DB)
@@ -227,7 +249,7 @@ def executeInsert(csvFileName):
 def main():
 
 	print("Inserting data")
-	executeInsert('lib/test_spreadsheet.csv')
+	execute_insert('lib/test_spreadsheet.csv')
 	print("Success: Data inserted into 3 tables!")
 
 if __name__ == "__main__":
