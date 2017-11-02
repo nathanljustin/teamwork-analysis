521455210ae380c2b0624194ff207017be4f4e9b
nathanljustin <njustin@g.hmc.edu>
2017-11-02 14:42:19 -0700

    Debugged bar graph files

--- teamworkApp/lib/findType.py
+++ teamworkApp/lib/findType.py
@@ -1,16 +1,11 @@
 def getType(scores):
     """ Find student's type(s) based on survey answers
     """
-    maxScore = 0
+    maxScore = max(scores)
     studentType = []
 
     # Get best scores
     for i in range(len(scores)):
         if scores[i] >= maxScore - 3:
             studentType += [i]
-        if scores[i] > maxScore:
-            maxScore = scores[i]
-            if scores[i] - 3 > maxScore:
-                studentType = [i]
-    
     return tuple(studentType)
