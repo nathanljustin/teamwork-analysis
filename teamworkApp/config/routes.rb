61d580d2b51d505c1bb43a302a73e7a5da099914
reagan-smith <reaganm.smith@yahoo.com>
2017-11-05 16:34:38 -0800

    Added first gen navbar and importData button

--- teamworkApp/config/routes.rb
+++ teamworkApp/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'high_voltage/pages#show', id: 'home'
   get '/python/studentGraph/' => "python#studentGraph"  
   get '/python/overallBar/' => 'python#overallBar'
+  get '/python/importData/' => 'python#importData'
   get '/studentGraph/' => 'high_voltage/pages#show', id: 'studentshow'
   get '/overallBar/' => 'high_voltage/pages#show', id: 'overallshow'
+  get '/importData/' => 'high_voltage/pages#show', id: 'home'
 end
