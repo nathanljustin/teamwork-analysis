7e0ba36eda0d63d2e265c6f49dbd83e8b3aab590
Maeve Murphy <mcmurphy@cs.hmc.edu>
2017-11-07 23:47:05 -0800

    Update routes.rb

--- teamworkApp/config/routes.rb
+++ teamworkApp/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'high_voltage/pages#show', id: 'home'
-  get '/python/studentGraph/' => "python#studentGraph"
   get '/python/overallBar/' => 'python#overallBar'
   get '/studentGraph/' => 'high_voltage/pages#show', id: 'studentshow'
   get '/overallBar/' => 'high_voltage/pages#show', id: 'overallshow'
