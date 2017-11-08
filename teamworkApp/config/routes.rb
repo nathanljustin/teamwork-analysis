d562d9eb65458831351b27825582bc9a92952666
Maeve Murphy <maevemurphy530@gmail.com>
2017-11-07 23:40:34 -0800

    added verification of args and removed changes

--- teamworkApp/config/routes.rb
+++ teamworkApp/config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
   root 'high_voltage/pages#show', id: 'home'
-<<<<<<< HEAD
-  resources :python do
-    collection do
-      get :overallPie
-    end
-  end
-=======
-  get '/python/studentGraph/' => "python#studentGraph"  
+  get '/python/studentGraph/' => "python#studentGraph"
   get '/python/overallBar/' => 'python#overallBar'
   get '/studentGraph/' => 'high_voltage/pages#show', id: 'studentshow'
   get '/overallBar/' => 'high_voltage/pages#show', id: 'overallshow'
->>>>>>> 8eb5a02c5f819de06edcac64304b2844cc0e8830
 end
