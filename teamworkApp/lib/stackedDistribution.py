c0d1cd8362fc65685e428c7a1d83d3e59d10dd3d
Maeve Murphy <maevemurphy530@gmail.com>
2017-11-15 23:00:43 -0800

    stacked graphs working

--- teamworkApp/lib/stackedDistribution.py
+++ teamworkApp/lib/stackedDistribution.py
@@ -7,7 +7,7 @@
 # of each type.
 
 # resources:
-# https://matplotlib.org/examples/statistics/histogram_demo_multihist.html
+# https://matplotlib.org/examples/pylab_examples/bar_stacked.html
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -18,11 +18,7 @@ DB = 'db/development.sqlite3'
 Style = IntEnum('Style', 'Communicator, Collaborator, Challenger, Contributor', start=0)
 
 def get_style_tuples():
-    """Returns a path to where the student's graph is saved"""
-
-
-
-    # Connect to db to get scores
+    """Retrieve all the style data stored in the database"""
     conn = sqlite3.connect(DB)
     c = conn.cursor()
     c.execute(
@@ -34,128 +30,79 @@ def get_style_tuples():
 
     return style_data
 
-def tally_primary(style_data):
+def tally_nth(style_data, n):
     """given a tuple of 4 scores: (communicator, collaborator,
     challenger, contributor), return tallies for how many
-    students in the DB have each as their primary"""
+    students in the DB have each as their nth order prominence of style.
+    For example, let n = 1 and get back the highest order prominence (primary)
+        style
+    NOTE: ties are settled such that they are counted in both categories of the
+    tie. For example, if the student's style was [45, 43, 43, 49] collaborator
+    would be 2nd and 3rd and challenger would *also* be 2nd and 3rd"""
     # communicator, collaborator, challenger, contributor
     tallies = [0,0,0,0]
     for student in style_data:
-        if student[0] == max(student):
+        # sort from low to high
+        sorted_styles = sorted(student)
+        # max is -1, second greatest is -2, etc.
+        if student[0] == sorted_styles[-n]:
             tallies[0] += 1
-        elif student[1] == max(student):
+        if student[1] == sorted_styles[-n]:
             tallies[1] += 1
-        elif student[2] == max(student):
+        if student[2] == sorted_styles[-n]:
             tallies[2] += 1
-        else:
+        if student[3] == sorted_styles[-n]:
             tallies[3] += 1
     return tallies
 
-def tally_secondary(style_data):
-    # communicator, collaborator, challenger, contributor
-    tallies = [0,0,0,0]
-    for student in style_data:
-        sorted_styles = sorted(student)
-        if student[0] == sorted_styles[-2]:
-            tallies[0] += 1
-        if student[1] == sorted_styles[-2]:
-            tallies[1] += 1
-        if student[2] == sorted_styles[-2]:
-            tallies[2] += 1
-        if student[3] == sorted_styles[-2]:
-            tallies[3] += 1
-    return tallies
-
-def tally_tertiary(style_data):
-    # communicator, collaborator, challenger, contributor
-    tallies = [0,0,0,0]
-    for student in style_data:
-        sorted_styles = sorted(student)
-        if student[0] == sorted_styles[-3]:
-            tallies[0] += 1
-        if student[1] == sorted_styles[-3]:
-            tallies[1] += 1
-        if student[2] == sorted_styles[-3]:
-            tallies[2] += 1
-        if student[3] == sorted_styles[-3]:
-            tallies[3] += 1
-    return tallies
-
-def tally_quaternary(style_data):
-    # communicator, collaborator, challenger, contributor
-    tallies = [0,0,0,0]
-    for student in style_data:
-        sorted_styles = sorted(student)
-        if student[0] == sorted_styles[0]:
-            tallies[0] += 1
-        if student[1] == sorted_styles[0]:
-            tallies[1] += 1
-        if student[2] == sorted_styles[0]:
-            tallies[2] += 1
-        if student[3] == sorted_styles[0]:
-            tallies[3] += 1
-    return tallies
 
 def make_stacked_graph(primaries, secondaries, tertiaries, quarternaries):
-    """graphs yay"""
-    # x_labels = [Style(x).name for x in range(len(Style))]
-    #
-    # n_bins = 4
-    # x = primaries, secondaries
-    # yPos = np.arange(len(x_labels))
-    #
-    # print(primaries)
-    #
-    # # fig, ax1 = plt.subplots(nrows=1, ncols=1)
-    # # ax1 = axes.flatten()
-    #
-    # # colors =  ['#3fe0d0', '#111e6c', '#008081', '#a3de38']
-    # # ax0.hist(x, n_bins, normed=1, histtype='bar', color=colors, label=colors)
-    # # ax0.legend(prop={'size': 10})
-    # # ax0.set_title('bars with legend')
-    #
-    # # plt.hist(x, n_bins, histtype='barstacked')#, color=colors)
-    # # plt.set_title('stacked bar')
-    # plt.hist(yPos, x, histtype='barstacked', align = 'center', alpha = 0.5)
-    # plt.xticks(yPos, x_labels)
-    # plt.ylabel("Score")
-    # plt.title("Student")
-    #
-    #
-    # # fig.tight_layout()
-    # plt.show()
+    """Given all the prominences of styles from the style table in the
+        database, create a stacked graph to aggregate the information
+        into one place by stacknig them in a histogram."""
+    x_labels = [Style(x).name for x in range(len(Style))]
 
+    # find the max height and scale the graph accordingly
+    heights = [0,0,0,0]
+    for i in range(0,4):
+        heights[i] = primaries[i] + secondaries[i] + tertiaries[i] + quarternaries[i]
+    max_y = max(heights)
+    increment = 1
+    # if the max is too big, split the ticks on the y axis for readablity
+    if max_y >= 30:
+        increment = 2
 
     N = 4
-    menMeans = (20, 35, 30, 35, 27)
-    womenMeans = (25, 32, 34, 20, 25)
-    menStd = (2, 3, 4, 1, 2)
-    womenStd = (3, 5, 2, 3, 3)
     ind = np.arange(N)    # the x locations for the groups
-    width = 0.35       # the width of the bars: can also be len(x) sequence
+    width = 0.35       # the width of the bars
 
-    p1 = plt.bar(ind, primaries, width, color='#d62728')
-    p2 = plt.bar(ind, secondaries, width,
-                 bottom=primaries)
-    p2 = plt.bar(ind, tertiaries, width,
-                  bottom=secondaries)
-    p2 = plt.bar(ind, quarternaries, width,
-               bottom=tertiaries)
+    # stack each ranking
+    # bottom is calculated such that it sits on top of rankings
+    # of higher order
+    prim  = plt.bar(ind, primaries, width, color='#82ccc4')
+    sec   = plt.bar(ind, secondaries, width, color='#777ea8',
+                  bottom=np.array(primaries))
+    tert  = plt.bar(ind, tertiaries, width, color='#008081',
+                  bottom=(np.array(secondaries) + np.array(primaries)))
+    quart = plt.bar(ind, quarternaries, width, color='#c4d89e',
+                  bottom=(np.array(secondaries)
+                  + np.array(tertiaries) + np.array(primaries)))
 
-    plt.ylabel('Scores')
-    plt.title('Scores by group and gender')
-    plt.xticks(ind, ('G1', 'G2', 'G3', 'G4', 'G5'))
-    plt.yticks(np.arange(0, 25, 10))
-    plt.legend((p1[0], p2[0]), ('Men', 'Women'))
+    plt.ylabel('Number of Students')
+    plt.title('Scores by Order of Styles')
+    plt.xticks(ind, x_labels)
+    plt.yticks(np.arange(0, max_y+2, increment))
+    plt.legend((prim[0], sec[0], tert[0], quart[0]),
+                    ('Primary', 'Secondary', 'Tertiary', 'Quaternary'))
 
     plt.show()
 
 def main():
     tuples        = get_style_tuples()
-    primaries     = tally_primary(tuples)
-    secondaries   = tally_secondary(tuples)
-    tertiaries    = tally_tertiary(tuples)
-    quarternaries = tally_quaternary(tuples)
+    primaries     = tally_nth(tuples, 1)
+    secondaries   = tally_nth(tuples, 2)
+    tertiaries    = tally_nth(tuples, 3)
+    quarternaries = tally_nth(tuples, 4)
 
     make_stacked_graph(primaries, secondaries, tertiaries, quarternaries)
 
