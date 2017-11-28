6f09f32684571306b15175e23ee1672fa74ae9a6
saramcallister <smcallister@g.hmc.edu>
2017-11-28 14:11:43 -0800

    Remove picture files along with data

--- teamworkApp/lib/delete_data.py
+++ teamworkApp/lib/delete_data.py
@@ -3,11 +3,24 @@
 # Last updated: 11-17-2017
 
 # delete all data from database (this is super sketch)
+
+import os
+
 import dbCalls
+
+summary_file = 'app/assets/images/summary.png'
+overall_file = 'app/assets/images/overall.png'
 
 def main():
     print('Deleting everything from students, styles, and answers.')
     dbCalls.remove_all()
+
+    # remove both summary and overall picture
+    try:
+        os.remove(summary_file)
+        os.remove(overall_file)
+    except OSError:
+        pass
 
 if __name__ == "__main__":
     main()
