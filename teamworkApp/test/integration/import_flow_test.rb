550eff123b3fa35be8821166b74d9ebe2d6177ed
saramcallister <smcallister@g.hmc.edu>
2017-12-02 14:03:39 -0800

    Added import non csv file check

--- teamworkApp/test/integration/import_flow_test.rb
+++ teamworkApp/test/integration/import_flow_test.rb
@@ -20,6 +20,25 @@ class ImportFlowTest < ActionDispatch::IntegrationTest
   	assert_equal 'Import was successful.', flash[:notice]
   end
 
+  test "cannot import non csv data" do
+    # start on the home page
+    get "/"
+    assert_select "h1", "Teamwork Analysis"
+
+    # test post method with non csv input
+    csv_upload = fixture_file_upload('files/.keep','text/csv')
+    post '/python/importData', params: {file: csv_upload}
+
+    # check correct redirection
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select "h1", "Teamwork Analysis"
+
+    # check for flash notice
+    assert_equal 'Import failed.', flash[:notice]
+  end
+
   test "can delete data" do 
   end
 end
